0b4e9cc3bc20b0d27f2affcca7c6921721fae031
pohanchi <aapp1420@gmail.com>
2019-09-26 18:28:20 +0800

    full version

--- code/GLUE_QA.py
+++ code/GLUE_QA.py
@@ -144,7 +144,81 @@ if __name__ == "__main__":
     input_length = min(input_length, model.config.n_positions)  # Max size of input for the pre-trained model
     tensor_datasets = pre_process_datasets(encoded_datasets, input_length,*special_tokens_ids)
 
+    train_data = TensorDataset(*tensor_datasets[0])
+    train_sampler = RandomSampler(train_data)
+    train_dataloader(train_data, sampler=train_sampler, batch_size=args.train_batch_size)
+
+    if args.do_train:
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            ]
+        num_train_optimization_steps = len(train_dataloader) * args.num_train_epochs
+        optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+        scheduler = WarmupLinearSchedule(optimizer, warmup_steps=args.warmup_steps, t_total=num_train_optimization_steps)
+
+    if args.fp16:
+        try:
+            from apex import amp
+        except ImportError:
+            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
 
 
+    if args.do_train:
+        writer = SummaryWriter(args.output_dir+"/tensorboard/")
+        nb_tr_steps, tr_loss, exp_average_loss = 0, 0, None
+        model.train()
+        if args.do_LLL != "":
+            importance = args.importance
+            Reg = Regularization(model=model,mode="EWC",dataloader=old_dataloader,device=device,optimizer=optimizer)
+        step_step = 1
+        for _ in trange(int(args.num_train_epochs), desc="Epoch"):
+            tr_loss = 0
+            nb_tr_steps = 0
+            tqdm_bar = tqdm(train_dataloader, desc="Training")
+            for step, batch in enumerate(tqdm_bar):
+                batch = tuple(t.to(device) for t in batch)
+                input_ids, lm_labels = batch
+                losses = model(input_ids, labels=lm_labels)
+                if args.do_LLL!="":
+                    loss = losses[0] + importance * Reg.penalty(model)
+                else:
+                    loss = losses[0]
 
+                if args.fp16:
+                    with amp.scale_loss(loss, optimizer) as scaled_loss:
+                        scaled_loss.backward()
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                
+                else:
+                    loss.backward()
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                    
+                optimizer.step()
+                scheduler.step()
+                writer.add_scalar('training_loss',loss,step_step)
+                optimizer.zero_grad()
+                tr_loss += loss.item()
+                step_step += 1
+                exp_average_loss = loss.item() if exp_average_loss is None else 0.7*exp_average_loss+0.3*loss.item()
+                nb_tr_steps += 1
+                tqdm_bar.desc = "Training loss: {:.2e} lr: {:.2e}".format(exp_average_loss, scheduler.get_lr()[0])
 
+    # Save a trained model
+    if args.do_train:
+        # Save a trained model, configuration and tokenizer
+        model_to_save = model.module if hasattr(model, 'module') else model  # Only save the model it-self
+        # If we save using the predefined names, we can load using `from_pretrained`
+        output_model_file = os.path.join(args.output_dir, WEIGHTS_NAME)
+        output_config_file = os.path.join(args.output_dir, CONFIG_NAME)
+        torch.save(model_to_save.state_dict(), output_model_file)
+        model_to_save.config.to_json_file(output_config_file)
+        tokenizer.save_pretrained(args.output_dir)
+        tokenizer.save_vocabulary(args.output_dir)
+        # Load a trained model and vocabulary that you have fine-tuned
+        model = OpenAIGPTLMHeadModel.from_pretrained(args.output_dir)
+        tokenizer = OpenAIGPTTokenizer.from_pretrained(args.output_dir)
+        model.to(device)

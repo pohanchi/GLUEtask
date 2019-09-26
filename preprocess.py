3460b19573c0e07d7282d19cf4b36865f38fde9f
pohanchi <aapp1420@gmail.com>
2019-09-26 16:43:16 +0800

    full preprocess

--- preprocess.py
+++ preprocess.py
@@ -7,13 +7,31 @@ import IPython
 import pdb 
 import json 
 import os 
-import apex 
+import pandas as pd  
 from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler, TensorDataset)
 from pytorch_transformers import (GPT2LMHeadModel, GPT2Tokenizer,GPT2Config,AdamW, cached_path, WEIGHTS_NAME, CONFIG_NAME, WarmupLinearSchedule)
-
+import pickle 
 
 
 
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    head = "./Dataset/"
+    parser.add_argument("--dataset_path",default="WNLI",type=str)
+    parser.add_argument("--mode",default="train",type=str,help="dev, train, test!!")
+    args=parser.parse_args()
+    
+    #choose mode
+    if args.mode == "train":
+        data=pd.read_csv(head + args.dataset_path+"/train.tsv",delimiter="\t",encoding='utf-8')
+    if args.mode == "dev":
+        data=pd.read_csv(head + args.dataset_path+"/dev.tsv",delimiter="\t",encoding='utf-8')
+    if args.mode == "test":
+        data=pd.read_csv(head + args.dataset_path+"/test.tsv",delimiter="\t",encoding='utf-8')
+    
+    predata = data.values[:,1:,].tolist()
+    print(len(predata))
+
+    
